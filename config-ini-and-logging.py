72f76055156676a96b94eb0ff0ff004d948f4153
Matthias Budde <buddemat@users.noreply.github.com>
2022-11-08 14:51:02 +0100

    Update and rename config-ini.py to config-ini-and-logging.py

---
+++ config-ini-and-logging.py
@@ -0,0 +1,25 @@
+import logging
+import yaml
+
+# Load options 
+options = yaml.safe_load(open('./config.yml'))
+
+# Initialize logging
+logger = logging.getLogger('mylogger')
+log_file_handler = logging.FileHandler(filename = options.get('logging').get('filename'), 
+                                       encoding = 'utf-8', 
+                                       mode = 'w')
+
+if options.get('logging').get('verbose'):
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    handlers = [log_file_handler, stdout_handler]
+else:
+    handlers = [log_file_handler]
+
+logging.basicConfig(
+         format = '%(asctime)s %(levelname)-8s %(message)s',
+         level = logging.INFO, # root logger
+         datefmt = '%Y-%m-%d %H:%M:%S',
+         handlers = handlers)
+
+logger.setLevel(logging.INFO) # mylogger
