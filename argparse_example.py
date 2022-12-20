44204a117d413099a313cdf11ec063c3039f9f55
Matthias Budde <buddemat@users.noreply.github.com>
2022-12-20 14:47:22 +0100

    Feat: Added short option to optional arguments.

--- argparse_example.py
+++ argparse_example.py
@@ -7,8 +7,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('foo', help="First named argument", default='bar')
 parser.add_argument('int', help="Named non-string argument (type: int)", type=int)
-parser.add_argument('--option', help="Optional argument")
-parser.add_argument('--flag', help="Optional flag, True when given, False otherwise", action="store_true")
+parser.add_argument('-o', '--option', help="Optional argument")
+parser.add_argument('-f', '--flag', help="Optional flag, True when given, False otherwise", action="store_true")
 args = parser.parse_args()
 for arg, val in vars(args).items():
     print(f'argument "{arg}" has the value "{val}"')
