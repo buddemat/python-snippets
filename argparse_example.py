9ee66f6e3c85094b3e1c303e618c4d686d41653c
Matthias Budde <buddemat@users.noreply.github.com>
2022-12-20 15:49:05 +0100

    Feat: Added verbosity flag.

--- argparse_example.py
+++ argparse_example.py
@@ -9,9 +9,14 @@ parser.add_argument('foo', help='first named/positional argument', default='bar'
 parser.add_argument('int', help='named/positional non-string argument (type: int)', type=int, choices=list(range(5)))
 parser.add_argument('-o', '--opt', help='optional argument')
 parser.add_argument('-f', '--flag', help='optional flag, True when given, False otherwise', action='store_true')
-parser.add_argument('-c', '--cnt', help='optional flag, counts occurences', action='count')
+parser.add_argument('-c', '--cnt', help='optional flag, counts occurences', action='count', default=0)
+parser.add_argument('-v', '--verbose', help='optional flag, counts verbosity', action='count', default=0)
 args = parser.parse_args()
-for arg, val in vars(args).items():
-    print(f'argument "{arg}" has the value "{val}"')
+
+if args.verbose >= 2:
+    print(f"Running '{__file__}'")
+if args.verbose >= 1:
+    for arg, val in vars(args).items():
+        print(f'...argument "{arg}" has the value "{val}"')
 
 
