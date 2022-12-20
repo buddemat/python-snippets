4457f3d836d22ae3091ed48215d868f1983f333a
Matthias Budde <buddemat@users.noreply.github.com>
2022-12-20 14:52:43 +0100

    Feat: Added range restriction on int arg.

--- argparse_example.py
+++ argparse_example.py
@@ -5,10 +5,10 @@ Script that reads arguments and prints them.
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('foo', help="First named argument", default='bar')
-parser.add_argument('int', help="Named non-string argument (type: int)", type=int)
-parser.add_argument('-o', '--option', help="Optional argument")
-parser.add_argument('-f', '--flag', help="Optional flag, True when given, False otherwise", action="store_true")
+parser.add_argument('foo', help="first named/positional argument", default='bar')
+parser.add_argument('int', help="named/positional non-string argument (type: int)", type=int, choices=list(range(5)))
+parser.add_argument('-o', '--opt', help="optional argument")
+parser.add_argument('-f', '--flag', help="optional flag, True when given, False otherwise", action="store_true")
 args = parser.parse_args()
 for arg, val in vars(args).items():
     print(f'argument "{arg}" has the value "{val}"')
