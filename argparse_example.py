d6ebdfae667de571b4159341fba12601b7789630
Matthias Budde <buddemat@users.noreply.github.com>
2022-12-20 14:54:52 +0100

    Feat: Added counting flag arg.

--- argparse_example.py
+++ argparse_example.py
@@ -5,10 +5,11 @@ Script that reads arguments and prints them.
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('foo', help="first named/positional argument", default='bar')
-parser.add_argument('int', help="named/positional non-string argument (type: int)", type=int, choices=list(range(5)))
-parser.add_argument('-o', '--opt', help="optional argument")
-parser.add_argument('-f', '--flag', help="optional flag, True when given, False otherwise", action="store_true")
+parser.add_argument('foo', help='first named/positional argument', default='bar')
+parser.add_argument('int', help='named/positional non-string argument (type: int)', type=int, choices=list(range(5)))
+parser.add_argument('-o', '--opt', help='optional argument')
+parser.add_argument('-f', '--flag', help='optional flag, True when given, False otherwise', action='store_true')
+parser.add_argument('-c', '--cnt', help='optional flag, counts occurences', action='count')
 args = parser.parse_args()
 for arg, val in vars(args).items():
     print(f'argument "{arg}" has the value "{val}"')
