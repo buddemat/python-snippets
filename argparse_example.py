a3380ff6547219c9af3f129aecc48c4dbd6c820d
Matthias Budde <buddemat@users.noreply.github.com>
2022-12-20 14:43:48 +0100

    Feat: Added optional arguments.

--- argparse_example.py
+++ argparse_example.py
@@ -5,8 +5,10 @@ Script that reads arguments and prints them.
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('foo', default='bar', help="First argument")
-parser.add_argument('int', type=int, help="Non-String argument")
+parser.add_argument('foo', help="First named argument", default='bar')
+parser.add_argument('int', help="Named non-string argument (type: int)", type=int)
+parser.add_argument('--option', help="Optional argument")
+parser.add_argument('--flag', help="Optional flag, True when given, False otherwise", action="store_true")
 args = parser.parse_args()
 for arg, val in vars(args).items():
     print(f'argument "{arg}" has the value "{val}"')
