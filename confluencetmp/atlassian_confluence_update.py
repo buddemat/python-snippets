5c7d831929536dc52c50707efc0761dda6594136
Matthias Budde <buddemat@users.noreply.github.com>
2023-01-23 13:48:12 +0100

    Feat: Added missing function definitions for connection.

--- confluencetmp/atlassian_confluence_update.py
+++ confluencetmp/atlassian_confluence_update.py
@@ -5,6 +5,24 @@ confluence_server = ''
 confluence_user = ''
 confluence_password = ''
 
+
+def confluence_connect(confluence_server, confluence_user, confluence_password):
+    try: 
+        c = Confluence(url=confluence_server, username=confluence_user, password=confluence_password)
+        # atlassian API does not raise an exception on failed login, so manual workaround to do this     
+        try:
+            c.get_user_details_by_username(confluence_user, expand=None)
+        except ApiPermissionError as err:
+            raise PermissionError('Permission error: Login failed.') from err
+        print('success!')
+        return c
+
+    except Exception as e:
+        print('  ... failed to connect to Confluence.')
+
+def confluence_site_search(conf_connection, query, q_type=None, q_space=None, limit=None):
+    pass
+
 # connect to server
 conf = confluence_connect(confluence_server, confluence_user, confluence_password)
 
@@ -19,4 +37,3 @@ page_type = 'page'
 
 # find pages
 page_ids = confluence_site_search(conf, query_string, page_type, page_space, find_limit)
-
